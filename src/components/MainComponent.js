f6f37541942b1b4131f568cd17ffda65186c559d
Septiana Yoga <septianayoga30@gmail.com>
2022-05-24 09:30:20 +0700

    adding leftside styling

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,37 @@
 import React from "react";
 
+import "./MainComponent.css";
+import { logoMain, iconDollar, iconPerson } from "../images";
+
 const MainComponent = () => {
-  return <div>MainComponent</div>;
+  return (
+    <div className="main-component-container">
+      <img src={logoMain} alt="logo" className="logo-main" />
+      <div className="app-container">
+        <div className="bill-form">
+          <h3>Bill</h3>
+          <img src={iconDollar} alt="dollar" className="icon-dollar" />
+          <input type="text" placeholder="0" />
+        </div>
+        <div className="tip-form">
+          <h3>Select Tip %</h3>
+          <div className="tip-container">
+            <span className="tip-value">5%</span>
+            <span className="tip-value">10%</span>
+            <span className="tip-value">15%</span>
+            <span className="tip-value">25%</span>
+            <span className="tip-value">50%</span>
+            <input type="text" className="tip-custom" placeholder="Custom" />
+          </div>
+        </div>
+        <div className="people-number">
+          <h3>Number of People</h3>
+          <img src={iconPerson} alt="person" className="icon-person" />
+          <input type="text" placeholder="0" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainComponent;
---
+++ src/images/index.js
@@ -0,0 +1,5 @@
+import logoMain from "./logo.svg";
+import iconDollar from "./icon-dollar.svg";
+import iconPerson from "./icon-person.svg";
+
+export { logoMain, iconDollar, iconPerson };
