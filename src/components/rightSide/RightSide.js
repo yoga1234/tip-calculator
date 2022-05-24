abb0c0a2c2bd1c4e73c5bdafed6357dc909429fe
Septiana Yoga <septianayoga30@gmail.com>
2022-05-24 13:06:36 +0700

    styling rightside

--- src/components/rightSide/RightSide.js
+++ src/components/rightSide/RightSide.js
@@ -6,15 +6,20 @@ const RightSide = () => {
   return (
     <div className="right-side-container">
       <div className="tip-amount">
-        <h3>Tip Amount</h3>
-        <p>/ Person</p>
+        <div className="left">
+          <h3>Tip Amount</h3>
+          <p>/ Person</p>
+        </div>
         <p className="total-money">$0.00</p>
       </div>
       <div className="total-amount">
-        <h3>Total</h3>
-        <p>/ Person</p>
+        <div className="left">
+          <h3>Total</h3>
+          <p>/ Person</p>
+        </div>
         <p className="total-money">$0.00</p>
       </div>
+      <button className="reset-button">RESET</button>
     </div>
   );
 };
